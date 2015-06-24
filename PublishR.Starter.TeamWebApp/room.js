6eddf89d9dfe5fa3e35af78659f67bc7fbee5828
Daniel McEwen <mcewen.daniel@gmail.com>
2015-06-24 18:02:22 +0100

    Initial room js and markup

--- PublishR.Starter.TeamWebApp/room.js
+++ PublishR.Starter.TeamWebApp/room.js
@@ -1,22 +1,126 @@
+var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
 var publishr;
 (function (publishr) {
     var starter;
     (function (starter) {
         "use strict";
-        var RoomController = (function () {
+        var cachedPage;
+        var CachedPageController = (function (_super) {
+            __extends(CachedPageController, _super);
+            function CachedPageController() {
+                _super.apply(this, arguments);
+            }
+            CachedPageController.prototype.getPage = function () {
+                if (cachedPage) {
+                    this.getPageSuccess(cachedPage);
+                }
+                else {
+                    _super.prototype.getPage.call(this);
+                }
+            };
+            CachedPageController.prototype.getPageSuccess = function (page) {
+                cachedPage = page;
+                _super.prototype.getPageSuccess.call(this, page);
+            };
+            CachedPageController.prototype.initialize = function () {
+                this.state.id = this.state['path'];
+                this.scope['state'] = this.state;
+                this.getPage();
+            };
+            return CachedPageController;
+        })(publishr.client.PageController);
+        var RoomController = (function (_super) {
+            __extends(RoomController, _super);
             function RoomController() {
+                _super.apply(this, arguments);
             }
             return RoomController;
-        })();
+        })(CachedPageController);
+        var SearchController = (function (_super) {
+            __extends(SearchController, _super);
+            function SearchController(scope, state, window, location, http, q, response) {
+                _super.call(this, scope, state, window, location, http, q);
+                this.scope = scope;
+                this.state = state;
+                this.window = window;
+                this.location = location;
+                this.http = http;
+                this.q = q;
+                this.response = response;
+            }
+            SearchController.prototype.initialize = function () {
+                var that = this;
+                this.search();
+                this.scope.$watch('result', function (newValue, oldValue) {
+                    that.response.setResponse(newValue);
+                });
+            };
+            SearchController.$inject = ["$scope", "$stateParams", "$window", "$location", "$http", "$q", "Response"];
+            return SearchController;
+        })(publishr.client.SearchController);
+        var CreateController = (function (_super) {
+            __extends(CreateController, _super);
+            function CreateController() {
+                _super.apply(this, arguments);
+            }
+            //initialize() {
+            //    super.initialize();
+            //}
+            CreateController.prototype.createPage = function (form) {
+                if (this.scope.create) {
+                    this.scope.create.kind = this.state.kind;
+                    this.scope.create.path = this.state["path"];
+                    this.scope.create.data.tags = [].concat(this.state["tag"]);
+                }
+                _super.prototype.createPage.call(this, form);
+            };
+            CreateController.prototype.createPageSuccess = function (resource) {
+                this.window.location.reload();
+            };
+            return CreateController;
+        })(CachedPageController);
         var states = function ($stateProvider, $urlRouterProvider) {
             $stateProvider.state('room', {
-                url: '/room',
-                controller: 'Room',
-                templateUrl: 'Room.html'
+                url: '/room/:path?kind&tag',
+                views: {
+                    "room": {
+                        templateUrl: "Room.html",
+                        controller: "Room"
+                    },
+                    "search": {
+                        templateUrl: "Search.html",
+                        controller: "Search"
+                    },
+                    "create": {
+                        templateUrl: "Create.html",
+                        controller: "Create"
+                    }
+                },
+                params: {
+                    path: 'default',
+                    kind: 'room',
+                    state: 'draft'
+                }
             });
-            $urlRouterProvider.otherwise("/room");
+            $urlRouterProvider.otherwise("/room/");
         };
-        angular.module('room', ['ui.router']).controller('Room', RoomController).config(['$stateProvider', '$urlRouterProvider', states]);
+        angular.module("room", ["ui.router"]).controller("Create", CreateController).controller("Room", RoomController).controller("Search", SearchController).factory("Response", function () {
+            var results = [];
+            results.data = null;
+            results.setResponse = function (response) {
+                results.data = response;
+                return results.data;
+            };
+            results.getResponse = function () {
+                return results.data;
+            };
+            return results;
+        }).config(["$stateProvider", "$urlRouterProvider", states]);
     })(starter = publishr.starter || (publishr.starter = {}));
 })(publishr || (publishr = {}));
 //# sourceMappingURL=room.js.map
--- PublishR.Starter.TeamWebApp/room.ts
+++ PublishR.Starter.TeamWebApp/room.ts
@@ -1,21 +1,124 @@
 ﻿module publishr.starter {
     "use strict";
 
-    class RoomController {
+    var cachedPage: publishr.client.Resource<publishr.client.Page>;
 
+    class CachedPageController extends publishr.client.PageController {
+
+        getPage() {
+            if (cachedPage) {
+                this.getPageSuccess(cachedPage);
+            }
+            else {
+                super.getPage();
+            }
+        }
+
+        getPageSuccess(page: publishr.client.Resource<publishr.client.Page>) {
+            cachedPage = page;
+
+            super.getPageSuccess(page);
+        }
+
+        initialize() {
+            this.state.id = this.state['path'];
+            this.scope['state'] = this.state;
+            this.getPage();
+        }
+
+    }
+
+    class RoomController extends CachedPageController {
+   
+    }
+
+    class SearchController extends publishr.client.SearchController {
+        constructor(
+            public scope: publishr.client.SearchScope,
+            public state: publishr.client.SearchState,
+            public window: ng.IWindowService,
+            public location: ng.ILocationService,
+            public http: ng.IHttpService,
+            public q: ng.IQService,
+            public response: Response
+            ) {
+            super(scope, state, window, location, http, q);
+        }
+        initialize() {
+            var that = this;
+            this.search();
+            this.scope.$watch('result', function (newValue, oldValue) {
+                that.response.setResponse(newValue);
+            });
+        }
+        static $inject = ["$scope", "$stateParams", "$window", "$location", "$http", "$q", "Response"];
+    }
+
+    class CreateController extends CachedPageController {
+        //initialize() {
+        //    super.initialize();
+        //}
+        createPage(form?: ng.IFormController) {
+            if (this.scope.create) {
+                this.scope.create.kind = this.state.kind;
+                this.scope.create.path = this.state["path"];
+                this.scope.create.data.tags = [].concat(this.state["tag"]);
+            }
+            super.createPage(form);
+        }
+        createPageSuccess(resource: publishr.client.Resource<publishr.client.Page>) {
+            this.window.location.reload();
+        }
     }
 
     var states = ($stateProvider: ng.ui.IStateProvider, $urlRouterProvider: ng.ui.IUrlRouterProvider) => {
         $stateProvider.state('room', {
-            url: '/room',
-            controller: 'Room',
-            templateUrl: 'Room.html'
+            url: '/room/:path?kind&tag',
+            views: {
+                "room": {
+                    templateUrl: "Room.html",
+                    controller: "Room"
+                },
+                "search": {
+                    templateUrl: "Search.html",
+                    controller: "Search"
+                },
+                "create": {
+                    templateUrl: "Create.html",
+                    controller: "Create"
+                }
+            },
+            params: {
+                path: 'default',
+                kind: 'room',
+                state: 'draft'
+            }
         });
-        $urlRouterProvider.otherwise("/room");
+        $urlRouterProvider.otherwise("/room/");
     };
 
+    interface Response {
+        getResponse(): publishr.client.Token;
+        setResponse(response): publishr.client.Token;
+        data: publishr.client.Token;
+    }
+
     angular
-        .module('room', ['ui.router'])
-        .controller('Room', RoomController)
-        .config(['$stateProvider', '$urlRouterProvider', states]);
+        .module("room", ["ui.router"])
+        .controller("Create", CreateController)
+        .controller("Room", RoomController)
+        .controller("Search", SearchController)
+        .factory("Response", function () {
+            var results : any = [];
+            results.data = null;
+            results.setResponse = function(response) {
+                results.data = response;
+                return results.data;
+            }
+            results.getResponse = function () {
+                return results.data;
+            }
+            return results;
+        })
+        .config(["$stateProvider", "$urlRouterProvider", states]);
 } 
